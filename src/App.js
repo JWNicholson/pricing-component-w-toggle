22a2e0e7490f4f53af58d50adab3f30bee8bab05
JW Nicholson <john.nicholson812@gmail.com>
2020-10-05 19:06:18 -0400

    basic app layout and background images done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Our Pricing</h1>
+       <p>[slider switch]</p>
       </header>
+      <main>
+       <h2>Main Content</h2>
+       <card>Card</card>
+       <card>Card</card>
+       <card>Card</card>
+      </main>
     </div>
   );
 }
